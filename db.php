434aa92500676563d2986f529cb2cdabcf0e1a8b
vbprofi <vb-profi@mail.ru>
2017-11-23 15:27:48 +0100

    setup for new database fixed

--- db.php
+++ db.php
@@ -1,23 +1,33 @@
 <?php
+//Datenbank
+$dbname = "kalender";
+//Host
+$dbhost = "localhost";
+//Benutzer
+$dbuser = "root";
+//Passwort
+$dbpass = "";
+
 $error = "";
-$mysqli = new mysqli("localhost","root","","kalender");
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 // Create connection
-$conn = new mysqli('localhost', 'root', '');
+$conn = new mysqli($dbhost, $dbuser, $dbpass);
  // Check connection
   if ($conn->connect_error) {
 $error .= "Connection failed: " . $conn->connect_error;
 } 
  
 // Create database
-  $sql = "CREATE DATABASE kalender";
+  $sql = "CREATE DATABASE ". $dbname;
 if ($conn->query($sql) === TRUE) {
 $error .= "Database created successfully";
 } else {
 $error .= "Error creating database: " . $conn->error;
 }
 
-//    die($error);
+//   die($error);
 
 $conn->close();
 ?>
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
 PHP Kalender v0.1<br>with adding, editing and deleting Events to MySQL DB.
+
+<h1>Installation</h1>
+<p><b>English:</b><br>
+1. edit db.php<br>
+2. run index.php<br>
+3. run setup.php <br>
+4. enjoy
+</p>
+<p><b>German:</b><br>
+1. passe die Datenbankverbindung in db.php an<br>
+2. führe index.php aus<br>
+3. führe  danach setup.php aus <br>
+4. viel Spaß
+</p>
