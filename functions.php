c394f55a52530696d934eb62c8c605e3a3d34a2d
vbprofi <vb-profi@mail.ru>
2017-12-11 00:38:32 +0100

    update mancy bugs...

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+//Datenbank einbinden
+include('db.php');
+
 /*
 *@Param datum
 * Beispielswert 2018-12-25
@@ -15,5 +18,10 @@ $wochentage = array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "
 $zeit = strtotime($datum);
 return $wochentage[date("w", $zeit)];
 }
+
+function string_input($string) {  
+global $mysqli;
+return trim(strip_tags($mysqli->real_escape_string($string)));
+} 
 
 ?>
